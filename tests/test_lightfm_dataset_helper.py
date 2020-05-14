445ddce0c1b325a211d03881c9d1d33aa6e956b6
Mohamed El Omari <Med-ELOMARI@users.noreply.github.com>
2020-05-14 19:40:10 +0200

    :white_check_mark:  Add Tests

--- tests/test_lightfm_dataset_helper.py
+++ tests/test_lightfm_dataset_helper.py
@@ -2,23 +2,14 @@
 
 """Tests for `lightfm_dataset_helper` package."""
 
-import pytest
+# from lightfm_dataset_helper import lightfm_dataset_helper
 
 
-from lightfm_dataset_helper import lightfm_dataset_helper
+from unittest import TestCase
 
+class TryTesting(TestCase):
+    def test_always_passes(self):
+        self.assertTrue(True)
 
-@pytest.fixture
-def response():
-    """Sample pytest fixture.
-
-    See more at: http://doc.pytest.org/en/latest/fixture.html
-    """
-    # import requests
-    # return requests.get('https://github.com/audreyr/cookiecutter-pypackage')
-
-
-def test_content(response):
-    """Sample pytest test function with the pytest fixture as an argument."""
-    # from bs4 import BeautifulSoup
-    # assert 'GitHub' in BeautifulSoup(response.content).title.string
+    def test_always_fails(self):
+        self.assertTrue(False)
