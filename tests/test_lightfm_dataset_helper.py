094b412f1cf470022cf2cbc52c77213dd75996bb
Mohamed El Omari <Med-ELOMARI@users.noreply.github.com>
2020-05-14 21:09:01 +0200

    :white_check_mark:  Test boilerplate TDD

--- tests/test_lightfm_dataset_helper.py
+++ tests/test_lightfm_dataset_helper.py
@@ -11,5 +11,5 @@ class TryTesting(TestCase):
     def test_always_passes(self):
         self.assertTrue(True)
 
-    def test_always_fails(self):
-        self.assertTrue(False)
+    # def test_always_fails(self):
+    #     self.assertTrue(False)
