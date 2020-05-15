a1849b099fd2514221e64af0dbdb055dec23f347
Mohamed El Omari <Med-ELOMARI@users.noreply.github.com>
2020-05-15 10:33:42 +0200

    Bump version: 0.1.0 → 0.1.1

--- setup.py
+++ setup.py
@@ -43,6 +43,6 @@ setup(
     test_suite='tests',
     tests_require=test_requirements,
     url='https://github.com/Med-ELOMARI/lightfm_dataset_helper',
-    version='0.1.0',
+    version='0.1.1',
     zip_safe=False,
 )
